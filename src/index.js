65a8a470715d70dcc91096d954a228114412adab
chendm <2283973011@qq.com>
2020-12-24 20:13:02 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import './style.css';
+
+function component() {
+  var element = document.createElement('div');
+  var btn = document.createElement('button');
+
+  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+  element.innerHTML = "Button HTML"
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+    var print = module.default
+    print()
+  });
+
+  element.appendChild(btn);
+
+  return element;
+}
+
+document.body.appendChild(component());
---
+++ webpack.common.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    chunkFilename: '[name].bundle.js',
+  },
+  plugins: [
+    new WebpackManifestPlugin(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({ title: "Webpack App!" }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  }
+};
